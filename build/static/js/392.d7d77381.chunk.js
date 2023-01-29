22ea30ece3367cea569bcdc6cd326e3af01408fd
harshal135patil <113527841+harshal135patil@users.noreply.github.com>
2023-01-30 02:17:05 +0530

    Delete build directory

--- build/static/js/392.d7d77381.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcodedeck=self.webpackChunkcodedeck||[]).push([[392],{7392:function(e,c,n){n.r(c);n(2791);var u=n(184);c.default=function(){return(0,u.jsx)("div",{children:"SignUp"})}}}]);
-//# sourceMappingURL=392.d7d77381.chunk.js.map
--- build/static/js/790.6494fe13.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcodedeck=self.webpackChunkcodedeck||[]).push([[790],{3790:function(e,c,n){n.r(c);n(2791);var u=n(184);c.default=function(){return(0,u.jsx)("div",{children:"Page404"})}}}]);
-//# sourceMappingURL=790.6494fe13.chunk.js.map
